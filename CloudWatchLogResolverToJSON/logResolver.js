fb6263e0b695ac842894e96069b41330be87a67a
Madhav Pujara <pujara.madhav@gmail.com>
2022-07-03 10:13:58 +0530

    added data

---
+++ CloudWatchLogResolverToJSON/logResolver.js
@@ -0,0 +1,6 @@
+let data = `[1.145.199.214] [07/Apr/2022:23:59:00 +0000] 
+[\"GET http://www.realestateview.com.au/real-estate/cnr-miller-road-
+bendigo-murchison-road-rushworth-vic/property-details-buy-residential-
+14049001/[logo]/ HTTP/:1.1\"] [301] [98] - [46.460] ms
+[[Mozilla/5.0 [Macintosh; Intel Mac OS X 10_13_6] AppleWebKit/605.1.15 [KHTML, like Gecko] Version/13.1.2 Safari/605.1.15]]
+[Datadome: 0-human-Humans] [pdp-buy] [4ee042d2-0ef3-4549-a187-e28ad1426837]`;
