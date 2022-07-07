90df49ae8bad9b20308e34fc777026fd0096e447
Madhav Pujara <pujara.madhav@gmail.com>
2022-07-07 19:18:47 +0530

    logResolver Revision

--- CloudWatchLogResolverToJSON/logResolver.js
+++ CloudWatchLogResolverToJSON/logResolver.js
@@ -1,15 +1,18 @@
 function Objectify(arr) {
   return {
-    id: arr[0],
+    ip: arr[0],
     date: arr[1],
-    request: arr[2],
-    respCode: arr[3],
-    ByteSize: arr[4],
-    time: `${arr[5]} ms`,
-    userAgent: arr[6],
-    dataDOM: arr[7],
-    promLabel: arr[8],
-    uuid: arr[9],
+    respCode: arr[2],
+    ByteSize: arr[3],
+    time: `${arr[4]} ${arr[5]}`,
+    dataDOM: arr[6],
+    promLabel: arr[7],
+    uuid: arr[8],
+    reqMethod: arr[9],
+    request: arr[10],
+    httpVersion: arr[11],
+    userAgent: arr[12],
+    referralUrl: arr[13],
   };
 }
 
@@ -32,28 +35,16 @@ function createArray(string) {
   return string.split(/\]\s\[|\s\-\s|\]\s[a-z]+\s\[/);
 }
 //Getting Data from User to make JSON
-let data = prompt(
-  "Enter String to be converted to JSON",
-  `[1.145.199.214] [07/Apr/2022:23:59:00 +0000] [\"GET http://www.realestateview.com.au/real-estate/cnr-miller-road-bendigo-murchison-road-rushworth-vic/property-details-buy-residential-14049001/[logo]/ HTTP/:1.1\"] [301] [98] - [46.460] ms [[Mozilla/5.0 [Macintosh; Intel Mac OS X 10_13_6] AppleWebKit/605.1.15 [KHTML, like Gecko] Version/13.1.2 Safari/605.1.15]] [Datadome: 0-human-Humans] [pdp-buy] [4ee042d2-0ef3-4549-a187-e28ad1426837]`
-);
+let data =
+  "[106.214.117.113] [04/Jul/2022:14:17:28 +0000] [301] [350] [0.570] [ms] [Datadome: 0-human-Humans] [redirect/zenu] [db1cc809-281a-420a-9bfa-244aa07dbc2d] [GET] [http://www.realestateview.com.au/real-estate/cnr-miller-road-bendigo-murchison-road-rushworth-vic/property-details-buy-residential-14049001/[logo] [HTTP/:1.1] [(Mozilla/5.0 (Linux; Android 10; Redmi Note 7S) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.101 Mobile Safari/537.36)] [undefined]";
 
-//Splitting String into Array
 let array = createArray(data);
 console.log(array);
 
-//Removing Square Brackets & datadom from arrays
 CleaningArray();
 
-//Making a object from cleanArray
 let dataObject = Objectify(array);
 
-//Making a JSON Object for network transfer
 let jsonString = JSON.stringify(dataObject);
 console.log(jsonString);
 
-//Making a JSON Object for Logging
-let jsonLog = JSON.stringify(dataObject, undefined, 2);
-console.log(jsonLog);
-
-alert(`JSON
-${jsonString}`);
